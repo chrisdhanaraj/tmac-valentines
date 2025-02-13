785f36b9d16a19457171a703ab2da13287f884e2
Chris Dhanaraj <cdhanaraj@netflix.com>
2025-02-13 12:57:46 -0800

    feat(card): add cards

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/src/app/page\": [\n      \"static/media/NewSpirit-s.p.5348b38b.ttf\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,188 @@
-import Image from "next/image";
+"use client";
+
+import * as React from "react";
+import NextImage, { StaticImageData } from "next/image";
+import localFont from "next/font/local";
+import bagelme from "./images/bagelme.png";
+import catchingvibes from "./images/catchingvibes.png";
+import doloisdry from "./images/doloisdry.png";
+import grandslam from "./images/grandslam.png";
+import heartrally from "./images/heart-rally.png";
+import linedolo from "./images/line-dolo.png";
+import lovelovenet from "./images/love-love-net.png";
+import niceace from "./images/niceace.png";
+import perfectmatch from "./images/perfectmatch.png";
+import servinglooks from "./images/servinglooks.png";
+import urcourt from "./images/urcourt.png";
+import vv from "./images/v&vv.png";
+
+const newSpirit = localFont({ src: "./font/NewSpirit.ttf" });
+
+const ImageButton = ({ src, setActiveImage, activeImage }: any) => {
+  return (
+    <NextImage
+      className={`border-2 ${
+        activeImage === src ? "border-green-800" : "border-transparent"
+      }`}
+      alt={"image"}
+      src={src}
+      width={100}
+      height={100}
+      onClick={() => {
+        setActiveImage(src);
+      }}
+    />
+  );
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [activeImage, setActiveImage] = React.useState(bagelme);
+  const [from, setFrom] = React.useState("");
+  const [to, setTo] = React.useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const generateCard = React.useCallback(
+    async (activeImage) => {
+      const canvas = document.createElement("canvas");
+      canvas.width = 1080;
+      canvas.height = 1398;
+
+      const context = canvas.getContext("2d");
+
+      console.log(activeImage);
+      const img = new Image();
+      img.src = activeImage.src;
+
+      img.onload = () => {
+        if (context == null) return;
+
+        context.drawImage(img, 0, 0);
+        context.font = `70px ${newSpirit.style.fontFamily}`;
+
+        context.fillStyle = "#166534";
+        context.fillText(from, 190, 1353);
+        context.fillText(to, 740, 1353);
+
+        const image = canvas.toDataURL("image/png");
+        const link = document.createElement("a");
+        link.download = `valentines.png`;
+        link.href = image;
+        link.click();
+      };
+    },
+    [from, to]
+  );
+
+  return (
+    <div
+      className={`${newSpirit.className} flex flex-col gap-4 py-4 max-w-[540px] mx-auto`}
+    >
+      <div className="flex justify-center">
+        <h1 className="text-3xl text-green-800">
+          TMAC Valentine Card Generator
+        </h1>
+      </div>
+      <div
+        className="flex flex-row gap-2 max-w-full p-4 overflow-auto [&::-webkit-scrollbar]:h-[4px]
+  [&::-webkit-scrollbar-track]:bg-gray-200
+  [&::-webkit-scrollbar-thumb]:bg-green-900"
+      >
+        <ImageButton
+          src={bagelme}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={catchingvibes}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={doloisdry}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={grandslam}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={heartrally}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={linedolo}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={lovelovenet}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={niceace}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={perfectmatch}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={servinglooks}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={urcourt}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+        <ImageButton
+          src={vv}
+          setActiveImage={setActiveImage}
+          activeImage={activeImage}
+        />
+      </div>
+      <div className="flex justify-center px-4">
+        <div className="relative">
+          <NextImage
+            src={activeImage}
+            width={500}
+            height={647}
+            alt="TMAC Card"
+          />
+          <div className="absolute bottom-[1.8%] left-[18%]">
+            <input
+              autoFocus
+              className="appearance-none bg-transparent text-green-800 text-2xl outline-none"
+              value={from}
+              onChange={(evt) => setFrom(evt.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="absolute bottom-[1.8%] right-[7%] w-[25%] ">
+            <input
+              className="appearance-none bg-transparent text-green-800 text-2xl outline-none w-full"
+              value={to}
+              onChange={(evt) => setTo(evt.target.value)}
+            />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      </div>
+
+      <div className="flex justify-center">
+        <button
+          className="bg-green-800 text-white px-4 py-2 rounded-md"
+          onClick={() => {
+            generateCard(activeImage);
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Download Card
+        </button>
+      </div>
     </div>
   );
 }
